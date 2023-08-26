5856ae9cbb46ada4959d4defeaae5b92c1df57d0
Aaron Chang <aaronchang153@gmail.com>
2023-08-25 23:58:36 -0700

    Minor refactor

--- server.go
+++ server.go
@@ -44,33 +44,40 @@ func HandleClient(conn net.Conn) {
         case int32(MSG_CLOSE):
             running = false
         case int32(MSG_TEXT):
-            //Next 4 bytes tell us the length of the message
-            msgLen := ReadInt32(conn)
+            err = HandleMsgText(conn)
+        default:
+            fmt.Println("Unrecognized message type. Client may be desynchronized.")
+            running = false
+        }
 
-            //Read the message string
-            msg := make([]byte, msgLen)
-            bytesRead := 0
-            for bytesRead < int(msgLen) {
-                n, err := conn.Read(msg[bytesRead:])
-                if err != nil {
-                    //TODO: this probably isn't good enough
-                    fmt.Println(err)
-                    running = false
-                }
-                bytesRead += n
-            }
-
-            ProcessMessage(conn.RemoteAddr().String(), string(msg[:msgLen]))
-
-            //Echo it back
-            _, err = conn.Write(msg)
-            if err != nil {
-                fmt.Println(err)
-                running = false
-            }
+        if err != nil {
+            fmt.Println(err)
+            running = false
         }
     }
     conn.Close()
+}
+
+func HandleMsgText(conn net.Conn) error {
+    //Next 4 bytes tell us the length of the message
+    msgLen := ReadInt32(conn)
+
+    //Read the message string
+    msg := make([]byte, msgLen)
+    bytesRead := 0
+    for bytesRead < int(msgLen) {
+        n, err := conn.Read(msg[bytesRead:])
+        if err != nil {
+            return err
+        }
+        bytesRead += n
+    }
+
+    ProcessMessage(conn.RemoteAddr().String(), string(msg[:msgLen]))
+
+    //Echo it back
+    _, err := conn.Write(msg)
+    return err
 }
 
 func ReadInt32(conn net.Conn) int32 {
