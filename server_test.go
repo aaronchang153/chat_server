b21e8dc3bcb36c60b8d408e463fa8d86dbc96986
Aaron Chang <aaronchang153@gmail.com>
2023-08-31 23:19:07 -0700

    Test concurrent connections

--- server_test.go
+++ server_test.go
@@ -16,25 +16,35 @@ var logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 func TestServer(t *testing.T) {
+	t.Parallel()
 	go RunServer(logger)
-
 	time.Sleep(1 * time.Second)
 
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "websocket"}
 
-	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal("dial:", err)
-	}
-	defer CloseConn(conn)
+	connections := make([]*websocket.Conn, 10)
+	for i := 0; i < 10; i++ {
+		conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+		if err != nil {
+			log.Fatal("dial:", err)
+		}
+		defer CloseConn(conn)
 
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello World!"))
-
-	_, buffer, err := conn.ReadMessage()
-	if err != nil {
-		log.Fatal("read:", err)
+		connections[i] = conn
 	}
-	logger.Println(string(buffer))
+
+	for _, conn := range connections {
+		conn.WriteMessage(websocket.TextMessage, []byte("Hello World!"))
+
+		_, buffer, err := conn.ReadMessage()
+		if err != nil {
+			log.Fatal("read:", err)
+		}
+		//logger.Println(string(buffer))
+		if string(buffer) != "Hello World!" {
+			logger.Fatal("Mismatch on echo operation")
+		}
+	}
 }
 
 func CloseConn(c *websocket.Conn) {
